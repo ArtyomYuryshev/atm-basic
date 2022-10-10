85d98b6a461436aa0f807a56ebf37175226d33ae
Artyom Yuryshev <artyom_yuryshev@epam.com>
2022-10-10 20:46:05 +0300

    module 2: added test for 'isNumberEven' method

--- module-2/tests/isNumberEven.test.js
+++ module-2/tests/isNumberEven.test.js
@@ -22,4 +22,32 @@ describe('isNumberEven', function () {
       validator.isNumberEven('4');
     }).to.throw('[4] is not of type "Number" it is of type "string"');
   });
+
+  it('should throw an error when provided an odd', function () {
+    expect(validator.isNumberEven(5)).to.be.equal(false);
+  });
+
+  it('should throw an error when provided a null', function () {
+    expect(() => {
+      validator.isNumberEven(null);
+    }).to.throw('[null] is not of type "Number" it is of type "object"');
+  });
+
+  it('should throw an error when provided with boolean', function () {
+    expect(() => {
+      validator.isNumberEven(true);
+    }).to.throw('[true] is not of type "Number" it is of type "boolean"');
+  });
+
+  it('should throw an errow when provided with an array', function () {
+    expect(() => {
+      validator.isNumberEven([2, 4]);
+    }).to.throw('[2,4] is not of type "Number" it is of type "object"');
+  });
+
+  it('should throw an errow when provided with an object', function () {
+    expect(() => {
+      validator.isNumberEven({ 1: 1 });
+    }).to.throw('[[object Object]] is not of type "Number" it is of type "object"');
+  });
 });
