d0fe39ff096e9037281b6ec1c29f468457bf98b4
Artyom Yuryshev <artyom_yuryshev@epam.com>
2022-10-10 14:12:41 +0300

    module 2: added test, coverage and lint scripts. added few tests. updated gitignore

--- module-2/tests/getEvenNumbersFromArray.test.js
+++ module-2/tests/getEvenNumbersFromArray.test.js
@@ -9,8 +9,18 @@ describe('getEvenNumbersFromArray', function () {
     validator = new NumbersValidator();
   });
 
+  afterEach(function () {
+    validator = null;
+  });
+
   it('should return an array of even numbers when provided with proper data', function () {
     const arrayOfRandomNumbers = [2, 7, 12, 17, 1, 55, 32, 10];
     expect(validator.getEvenNumbersFromArray(arrayOfRandomNumbers)).to.be.eql([2, 12, 32, 10]);
+  });
+
+  it('should throw an error when provided not a number in array', function () {
+    expect(() => {
+      validator.getEvenNumbersFromArray([4, 8, 15, 16, '"twenty three"', 42]);
+    }).to.throw('[4,8,15,16,"twenty three",42] is not an array of "Numbers"');
   });
 });
