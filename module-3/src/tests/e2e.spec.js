1541cbbd5be46edb38a1f52cc63b4c6f513362ff
Artyom Yuryshev <artyom_yuryshev@epam.com>
2022-10-28 17:41:42 +0300

    module 3: fix comments v2

--- module-3/src/tests/e2e.spec.js
+++ module-3/src/tests/e2e.spec.js
@@ -1,6 +1,6 @@
 describe('Module 3: WebdriverIO Introduction', () => {
   it('1st test: check page title', async () => {
-    await browser.url('https://ej2.syncfusion.com/showcase/angular/appointmentplanner/#/dashboard');
+    await browser.url('/showcase/angular/appointmentplanner/#/dashboard');
     const pageTitle = await browser.getTitle();
     console.log(pageTitle);
     expect(pageTitle).toEqual('Appointment Planner - Syncfusion Angular Components Showcase App');
@@ -8,7 +8,7 @@ describe('Module 3: WebdriverIO Introduction', () => {
 
   it('2nd test: check empty name validation', async () => {
     await (await $('//*[@class="sidebar-item doctors"]')).click();
-    await (await $("//*[@class='specialization-types']//button")).click();
+    await (await $("//*[@class='specialization-types']//button[text()='Add New Doctor']")).click();
     await (await $("//*[@for='patientCheckFemale']")).click();
     const emailError = await $("//*[@id='Name-info']");
     expect(await emailError.getText()).toEqual('Enter valid name');
@@ -22,20 +22,20 @@ describe('Module 3: WebdriverIO Introduction', () => {
   });
 
   it('4th test: close create doctor pop-up', async () => {
-    const popUp = await $("ejs-dialog[cssclass='new-doctor-dialog']");
-    await expect(popUp).toExist(true);
-    await (await $("div[class='button-container']>button:nth-child(1)")).click();
-    await expect(popUp).toExist(false);
+    const popUp = await $("body > div[style='z-index: 1000; display: flex; position: fixed;']");
+    await expect(popUp).toExist();
+    await (await $("div[class='button-container']>button[class='e-control e-btn e-lib e-normal']")).click();
+    await await expect(popUp).not.toExist();
   });
 
   it('5th test: create a new doctor', async () => {
-    await (await $('div.specialization-types>button')).click();
+    await (await $('//*[@class="specialization-types"]//..//button["Add New Doctor"]')).click();
     await (await $("input[name='Name']")).setValue('Lol Kek');
     await (await $('input#DoctorMobile')).setValue('1234567890');
     await (await $("input[name='Email']")).setValue('Lol@Kek.com');
     await (await $("input[name='Education']")).setValue('brodyaga');
-    await $("div[class='button-container']>button:nth-child(2)").click();
-    const newDoc = await $("div#Specialist_8 div[class='name']");
+    await $("//*[@class='e-footer-content']//button[@class='e-control e-btn e-lib e-normal e-primary' and 'Save']").click();
+    const newDoc = await $('#Specialist_8 .name');
     expect(await newDoc.getText()).toEqual('Dr. Lol Kek');
   });
 });
