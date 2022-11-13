f5c8d2fc92f3ff95901f147fd9bea9aea4aa49bd
Artyom Yuryshev <artyom_yuryshev@epam.com>
2022-11-13 14:56:06 +0000

    Module 6: convert tests in PO

---
+++ module-6/src/po/components/doctors/break-hours.component.js
@@ -0,0 +1,21 @@
+const BaseComponent = require('../common/base.component');
+
+class BreakHoursModal extends BaseComponent {
+  constructor() {
+    super('div>.e-popup-open');
+  }
+
+  get saveBtn() {
+    return this.rootEl.$('.e-popup-open .button-container button.e-primary');
+  }
+
+  get sundayBtn() {
+    return this.rootEl.$('#sunday_button');
+  }
+
+  get fridayBtn() {
+    return this.rootEl.$('#friday_button');
+  }
+}
+
+module.exports = BreakHoursModal;
